820db273a7807b9d8ab73b78190f97de22974769
Michael Tobechukwu <76704861+Michael-Tobechukwu@users.noreply.github.com>
2021-08-21 12:15:12 +0100

    Add files via upload
    A simple JavaScript program.

---
+++ namescript.js
@@ -0,0 +1,4 @@
+alert("Hi, I'm @DevTobechukwu, Michael Tobechukwu Nwokike");
+alert("I teach tech. I build webpages. I write copies.");
+let name = prompt("What is your name?");
+alert("You are welcome, " + name + "!");
